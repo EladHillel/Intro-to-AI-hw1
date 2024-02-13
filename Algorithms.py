d596e0329eb5fbb83406989dd51c4bc8f7bff9ae
Y-Berman <yair.berman@campus.technion.ac.il>
2024-02-13 19:48:56 +0200

    my code

--- Algorithms.py
+++ Algorithms.py
@@ -15,29 +15,52 @@ class Node():
 
 class BFSAgent():
     def __init__(self) -> None:
-        self.open = deque([])
+        self.openNodes = deque([])
         self.closed = {}
-        self.nodes = {}
+        self.nodes = dict() # state: Node(state, prevState, action)
         self.expanded = 0
 
     def search(self, env: DragonBallEnv) -> Tuple[List[int], float, int]:
         startState = env.get_initial_state()
         self.open.append(startState)
+        startNode = Node((startState), None, None)
+        self.nodes[currentState] = startNode
         while len(self.open):
             currentState = self.open.popleft()
+            self.expanded += 1
             self.closed.add(currentState)
-
             for action, successor in env.succ():
                 env.set_state(currentState)
                 childState = env.step(action)
                 if env.is_final_state(childState):
-                    # return something
-                    pass
+                    steps = retrace_steps(currentState)
+                    steps.append(action)
+                    cost = calculate_cost(steps)
+                    return (steps, cost, self.expanded)
                 if (not successor[2]) and (childState not in self.closed) and (childState not in self.open):
                     self.open.append(childState)
+                    self.nodes[childState] = Node(childState, currentState, action)
 
         # return failure
-
+        def retrace_steps(finalState: Tuple) -> list[int]:
+            actions = []
+            while self.nodes[finalState][2] != None:
+                actions.append(self.nodes[finalState][2])
+                finalState = self.nodes[finalState][1]
+            actions.reverse()
+            return actions
+        def calculate_cost(actions: list[int]) -> int:
+            # set initial and than walk by the actions
+            state = env.get_initial_state()
+            env.set_state(state)
+            cost = 0
+            index = 0
+            terminated = False
+            while terminated != True:
+                state, tempCost, terminated = env.step(actions[index])
+                index += 1
+                cost += tempCost
+            return cost
 
 
 
